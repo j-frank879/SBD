65565b9fc237021ec09506db09b822bae2873865
Joanna Frankowska <j.frankowska8844@gmail.com>
2023-01-24 21:57:10 +0100

    wiadomosc

--- SBD/src/main/java/com/example/entity/Wiadomosc.java
+++ SBD/src/main/java/com/example/entity/Wiadomosc.java
@@ -1,17 +1,82 @@
 package com.example.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+@Entity
+@Table(name = "wiadomosc")
 public class Wiadomosc
 {
+    public Wiadomosc() {
+        this.dataWyslania =new Date();
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Long id;
-    Date dataWyslania;
-    String tresc;
-    String nazwaNadawcy; //fk
-    String nazwaOdbiorcy; //fk
+    private Integer id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    @Column
+    private Date dataWyslania;
+    @Column(length=150)
+    private String tresc;
+    @Column(length=30)
+    private String nazwaNadawcy;
+    @Column(length=30)
+    private String nazwaOdbiorcy;
+
+    public Wiadomosc(Integer id, Date dataWyslania, String tresc, String nazwaNadawcy, String nazwaOdbiorcy) {
+        this.id = id;
+        this.dataWyslania = dataWyslania;
+        this.tresc = tresc;
+        this.nazwaNadawcy = nazwaNadawcy;
+        this.nazwaOdbiorcy = nazwaOdbiorcy;
+    }
+    public Wiadomosc(Integer id,  String tresc, String nazwaNadawcy, String nazwaOdbiorcy) {
+
+        this.id = id;
+        this.dataWyslania =new Date();
+        this.tresc = tresc;
+        this.nazwaNadawcy = nazwaNadawcy;
+        this.nazwaOdbiorcy = nazwaOdbiorcy;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDataWyslania() {
+        return dataWyslania;
+    }
+
+    public void setDataWyslania(Date dataWyslania) {
+        this.dataWyslania = dataWyslania;
+    }
+
+    public String getTresc() {
+        return tresc;
+    }
+
+    public void setTresc(String tresc) {
+        this.tresc = tresc;
+    }
+
+    public String getNazwaNadawcy() {
+        return nazwaNadawcy;
+    }
+
+    public void setNazwaNadawcy(String nazwaNadawcy) {
+        this.nazwaNadawcy = nazwaNadawcy;
+    }
+
+    public String getNazwaOdbiorcy() {
+        return nazwaOdbiorcy;
+    }
+
+    public void setNazwaOdbiorcy(String nazwaOdbiorcy) {
+        this.nazwaOdbiorcy = nazwaOdbiorcy;
+    }
 }
---
+++ SBD/src/main/java/com/example/repository/WiadomoscRepository.java
@@ -0,0 +1,19 @@
+package com.example.repository;
+
+import java.util.List;
+
+
+
+import com.example.entity.Wiadomosc;
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+@Transactional
+public interface WiadomoscRepository extends JpaRepository<Wiadomosc, Integer> {
+    List<Wiadomosc> findByNazwaOdbiorcyContainingIgnoreCase(String keyword);
+}
